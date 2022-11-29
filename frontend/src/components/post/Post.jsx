f2740462a3ac084d592c3b38d1d2d59eaff60598
bipularyal <59837845+bipularyal@users.noreply.github.com>
2022-11-28 18:55:24 -0600

    errors with frontend timeago.js fixed. NProject not working

--- frontend/src/components/post/Post.jsx
+++ frontend/src/components/post/Post.jsx
@@ -2,7 +2,7 @@ import "./post.css";
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { useContext, useEffect, useState } from "react";
 import axios from "axios";
-import { format } from "timeago.js";
+// import { format } from "timeago.js";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
 
@@ -55,7 +55,7 @@ export default function Post({ post }) {
               {user.username}
               </span>
             <span className="postDate">
-              {format(post.createdAt)}
+              {post.createdAt}
               </span>
           </div>
           <div className="postTopRight">
